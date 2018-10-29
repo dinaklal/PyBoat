00783fae8359a2e2a44e437b52cc382f38dbb1c4
dinaklal <dinaklal@gmail.com>
2018-10-29 12:17:12 +0000

    Basic greeting

---
+++ Main.py
@@ -0,0 +1,5 @@
+
+from scripts.greeting import check_for_greeting
+
+sentence = input(" ")
+print(check_for_greeting(sentence))
---
+++ scripts/greeting.py
@@ -0,0 +1,10 @@
+import random
+# Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+
+def check_for_greeting(sentence):
+    if sentence.lower() in GREETING_KEYWORDS:
+            return random.choice(GREETING_RESPONSES)
+            
