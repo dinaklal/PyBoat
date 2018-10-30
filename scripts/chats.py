ecb84345b72b54869cefefa6f9dee635355a2024
dinaklal <dinaklal@gmail.com>
2018-10-30 11:39:04 +0000

    random chitchats

--- scripts/chats.py
+++ scripts/chats.py
@@ -3,12 +3,16 @@ name = "PyBot"
 weather = "Cloudy"
 # Sentences we'll respond with if the user talks
 responses = {
-  "what's your name?": "my name is {0}".format(name),
-  "what's today's weather?": "the weather is {0}".format(weather),
-  "default": "default message"
+  "what's your name?": ["my name is {0}".format(name),
+    "they call me {0}".format(name),
+      "I go by {0}".format(name)],
+  "what's today's weather?": ["the weather is {0}".format(weather),"it's {0} today".format(weather)],
+  "default": ["Sorry,Not Getting your language", "What are you talking",""]
 }
 
 def chats(sentence):
     if sentence.lower() in responses:
-            return "PyBot : " + responses[ sentence ]
+            return "PyBot : " + random.choice(responses[ sentence ])
+    else:
+         return "PyBot : " + random.choice(responses["default"])
             
